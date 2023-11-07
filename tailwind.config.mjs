50c4a513bbef1434e18559481b59eff59267c574
m4nute <mgudinocontacto@gmail.com>
2023-11-07 20:19:17 -0300

    Add shadow to Navbar and create shine animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        bright: "0 0 10px rgba(255, 255, 255, 0.5)",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
